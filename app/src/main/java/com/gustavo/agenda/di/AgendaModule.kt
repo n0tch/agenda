76c52e9b3cf84c9aad07f7e20845469522378c49
Gustavo <gusoliv3ira@gmail.com>
2023-03-05 10:42:21 -0300

    update event detail module injection

--- app/src/main/java/com/gustavo/agenda/di/AgendaModule.kt
+++ app/src/main/java/com/gustavo/agenda/di/AgendaModule.kt
@@ -1,6 +1,6 @@
 package com.gustavo.agenda.di
 
-import com.gustavo.agenda.eventDate.EventDateViewModel
+import com.gustavo.agenda.eventDate.presentation.EventDateViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
