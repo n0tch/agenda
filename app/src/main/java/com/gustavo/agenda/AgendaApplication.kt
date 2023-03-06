32df63f8c29e9de0331e125d8d1a0f5ebc395728
Gustavo <gusoliv3ira@gmail.com>
2023-03-06 10:53:45 -0300

    include all modules on stark koin

--- app/src/main/java/com/gustavo/agenda/AgendaApplication.kt
+++ app/src/main/java/com/gustavo/agenda/AgendaApplication.kt
@@ -2,7 +2,9 @@ package com.gustavo.agenda
 
 import android.app.Application
 import com.gustavo.agenda.di.agendaModule
+import com.gustavo.agenda.di.appModule
 import com.gustavo.agenda.di.eventModule
+import com.gustavo.agenda.di.notificationModule
 import org.koin.core.context.startKoin
 import org.koin.android.ext.koin.androidContext
 
@@ -13,6 +15,8 @@ class AgendaApplication: Application() {
         startKoin {
             androidContext(this@AgendaApplication)
             modules(listOf(
+                appModule,
+                notificationModule,
                 agendaModule,
                 eventModule
             ))
