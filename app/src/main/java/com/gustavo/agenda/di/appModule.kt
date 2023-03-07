83f3729eebae623cb736683e8459b2f575a097d7
Gustavo <gusoliv3ira@gmail.com>
2023-03-07 18:16:45 -0300

    reorganize modules variables

--- app/src/main/java/com/gustavo/agenda/di/appModule.kt
+++ app/src/main/java/com/gustavo/agenda/di/appModule.kt
@@ -2,7 +2,7 @@ package com.gustavo.agenda.di
 
 import com.google.gson.Gson
 import com.gustavo.agenda.common.repository.AgendaPreferences
-import com.gustavo.agenda.eventDetail.domain.AgendaEventMapper
+import com.gustavo.agenda.data.transformation.AgendaEventMapper
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
